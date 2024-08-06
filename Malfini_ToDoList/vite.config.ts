f63417e575a6606f239122d83981fd59ec642043
Radek Myška <radek.myska@idealine.cz>
2024-08-06 21:49:50 +0200

    build and deploy to firebase configuration

--- Malfini_ToDoList/vite.config.ts
+++ Malfini_ToDoList/vite.config.ts
@@ -4,14 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-      "@components": "/src/components",
-      "@pages": "/src/pages",
-      "@services": "/src/services",
-      "@context": "/src/context",
-      "@constants": "/src/constants",
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+      scopeBehaviour: "local",
     },
   },
+  // resolve: {
+  //   alias: {
+  //     "@": "/src",
+  //     "@components": "/src/components",
+  //     "@pages": "/src/pages",
+  //     "@services": "/src/services",
+  //     "@context": "/src/context",
+  //     "@constants": "/src/constants",
+  //   },
+  // },
 });
