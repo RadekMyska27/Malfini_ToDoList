05f423fd8cd9d27251b7e84c3b13e7dd8484698f
Radek Myška <radek.myska@idealine.cz>
2024-08-06 12:05:37 +0200

    styles

--- Malfini_ToDoList/src/pages/todoListPage.tsx
+++ Malfini_ToDoList/src/pages/todoListPage.tsx
@@ -1,10 +1,8 @@
 ﻿import { Center, Flex, Space } from "@mantine/core";
-import styles from "./styles.module.css";
 import ToDosList from "../components/toDosList/toDosList.tsx";
 import AppHeader from "../components/headerWithAddButton/appHeader.tsx";
 import AddTaskSection from "../components/addTaskSection/addTaskSection.tsx";
-
-export interface ITodoListData {}
+import styles from "./styles.module.css";
 
 const TodoListPage = () => {
   return (
